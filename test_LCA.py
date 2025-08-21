1b15b6003303507b398f1a7021a51eb7c96556d0
Keyvan <71347921+Keyvantajfar@users.noreply.github.com>
2025-08-21 13:44:24 -0700

    new project

--- test_LCA.py
+++
@@ -1,4 +0,0 @@
-# import lca_pipeline as lca
-import LCA_GPT as lca
-results = lca.run_lca('20250408-dsRNA in vitro synthesis-LCA calculation.xlsx', write_back=True)
-# results is a dict: {'U_1_Midpoint': df, 'U_2_Midpoint': df, ...}
